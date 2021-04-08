eb41d44b2a66db887ba9911b16cb6e90097804ca
eugenemp <phoenixlp91@mail.ru>
2021-04-08 20:33:25 +0300

    feat: implement deleteDigit function

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,17 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const str = n.toString();
+  const result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    // At every iteration cut one digit at current index and push
+    // result string to result array
+    result.push(str.slice(0, i) + str.slice(i + 1));
+  }
+  // Get maximal number from result array
+  return Math.max(...result);
 }
 
 module.exports = deleteDigit;
