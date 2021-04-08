248590a3a76fa70e7f34bde92ca3c4260c105839
eugenemp <phoenixlp91@mail.ru>
2021-04-09 01:18:46 +0300

    feat: implement findIndex function

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,23 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let maxIndex = array.length - 1;
+  let minIndex = 0;
+  let currentIndex = 0;
+  // Implement binary search algorithm
+  while (array[currentIndex] !== value) {
+    // Find middle element in whole array or range of elements
+    currentIndex = Math.round((minIndex + maxIndex) / 2);
+    // If value at the middle of array/range is lower than
+    // target value - search in higher half
+    if (array[currentIndex] < value) minIndex = currentIndex + 1;
+    // If value at the middle of array/range is higher than
+    // target value - search in lower half
+    if (array[currentIndex] > value) maxIndex = currentIndex - 1;
+  }
+  // Once middle element equals target value - return it's index
+  return currentIndex;
 }
 
 module.exports = findIndex;
