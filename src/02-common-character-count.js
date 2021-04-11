1e247a1d322325da7b2ee2e6bcae11103ff04d1a
eugenemp <phoenixlp91@mail.ru>
2021-04-11 18:40:37 +0300

    feat: implemet getCommonCharacterCount function

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,21 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  // Split second string to use array's splice method
+  const str2 = s2.split('');
+
+  for (let i = 0; i < s1.length; i++) {
+    // If letter is common between s1 and s2, delete it from s2
+    // so it wiil not be counted again
+    const index = str2.indexOf(s1[i]);
+    if (index >= 0) {
+      str2.splice(index, 1);
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = getCommonCharacterCount;
