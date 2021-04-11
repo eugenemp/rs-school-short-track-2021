1a6fbf12039e2fb62b273a6c97cd19eae7c00dae
eugenemp <phoenixlp91@mail.ru>
2021-04-11 22:51:10 +0300

    feat: implement isMAC48Address function

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,15 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const tempArr = n.split('-');
+  // Define regex pattern to check if digits are hexadecimal
+  const regex = /^[0-9a-fA-F]{2,}/;
+  // Check every group of mac address to match regex
+  for (let i = 0; i < tempArr.length; i++) {
+    if (!regex.test(tempArr[i])) return false;
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
