04a31c89a5588afbff69da306f2059be2490319b
eugenemp <phoenixlp91@mail.ru>
2021-04-12 19:35:32 +0300

    feat: implement getDNSStats function

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,34 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const result = {};
+  // Define regex to find each level's domain
+  const tld = /\.\w*$/;
+  const sld = /\w*(?=\.\w*$)/;
+  const rld = /\w*(?=\.\w*\.\w*$)/;
+
+  domains.forEach((domain, i) => {
+    const first = domains[i].match(tld);
+    const secnd = domains[i].match(sld);
+    const third = domains[i].match(rld);
+    // Add top domain to result obj or increase count if it already exist
+    if (first) {
+      if (!result[first]) result[first] = 1;
+      else result[first]++;
+    }
+    // Add second level domain to result obj or increase count if it already exist
+    if (secnd) {
+      if (!result[`${first}.${secnd}`]) result[`${first}.${secnd}`] = 1;
+      else result[`${first}.${secnd}`]++;
+    }
+    // Add second level domain to result obj or increase count if it already exist
+    if (third) {
+      if (!result[`${first}.${secnd}.${third}`]) result[`${first}.${secnd}.${third}`] = 1;
+      else result[`${first}.${secnd}.${third}`]++;
+    }
+  });
+  return result;
 }
 
 module.exports = getDNSStats;
