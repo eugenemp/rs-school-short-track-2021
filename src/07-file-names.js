2b107b18b11ea51d07d843751cdecdcb60eac312
eugenemp <phoenixlp91@mail.ru>
2021-04-12 22:47:15 +0300

    feat: implement renameFiles function

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,31 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  // Use object to count the number that each name repeats
+  const counts = {};
+  const output = [];
+
+  for (let i = 0; i < names.length; i++) {
+    // If current name not in count object - pass it to output without postfix
+    if (!counts[names[i]]) {
+      // Also check if the name has postfix as part of it,
+      // if so - check if it not conflicts with the names in output
+      if (output.includes(names[i])) {
+        counts[names[i]] = 1;
+        output.push(`${names[i]}(${counts[names[i]]})`);
+      } else {
+        counts[names[i]] = 1;
+        output.push(names[i]);
+      }
+      // If name is already in object - add postfix with it's count
+    } else if (counts[names[i]]) {
+      output.push(`${names[i]}(${counts[names[i]]})`);
+      counts[names[i]]++;
+    }
+  }
+
+  return output;
 }
 
 module.exports = renameFiles;
