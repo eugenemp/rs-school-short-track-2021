0bf94a34b98e12da05a4f719a52ace6fd1d0bac7
eugenemp <phoenixlp91@mail.ru>
2021-04-12 04:52:09 +0300

    feat: encodeLine function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    // If next char is tha same - increase counter
+    if (str[i] === str[i + 1]) count++;
+    // If next char is not the same - check counter
+    if (str[i] !== str[i + 1]) {
+      count++;
+      // If repeat count is more then one - add num in front of char
+      if (count > 1) {
+        result = result.concat(`${count}${str[i]}`);
+      } else {
+        result = result.concat(`${str[i]}`);
+      }
+      count = 0;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
